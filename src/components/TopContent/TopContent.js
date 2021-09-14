823efc53aec32e88a4a8312fad76502420113b1e
Yokesh Raj <yokeshh98@gmail.com>
2021-09-14 14:45:35 +0530

    resume added

--- src/components/TopContent/TopContent.js
+++ src/components/TopContent/TopContent.js
@@ -9,7 +9,7 @@ const TopContent = () => {
         <h1>Mr.Yokeshwar Raj</h1>
         <p>A Professional Full Stack Developer</p>
         <a
-          href="https://doc-0s-3s-docs.googleusercontent.com/docs/securesc/7f30l5866mkqacm2msppsn2qqeq63tlk/pedb1k9em2sr5ok6kqgkjvmu55vn5mk7/1631601825000/08124396825101469741/08124396825101469741/1WSWp4s_-gi2eTUNv7DYYWuPZflUz63pu?e=download&authuser=0&nonce=mbgv54id6hrci&user=08124396825101469741&hash=madcfn9jtngkvdj4dvltcn0a9vlocnvu"
+          href="https://mail.google.com/mail/u/0?ui=2&ik=229bda3c71&attid=0.1&permmsgid=msg-a:r2678336536277390252&view=att&disp=safe&realattid=f_ktjuy0nx0"
           target="_blank"
           rel="noopener noreferrer"
         >
